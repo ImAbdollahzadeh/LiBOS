1589a2245c70ff2947d8f608e0642856960fbb6b
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2019-12-28 20:03:40 +0100

    Add files via upload

--- include/WINDOW.h
+++ include/WINDOW.h
@@ -12,8 +12,8 @@ enum {
 };
 
 typedef struct _POINT {
-	UINT_32 x;
-	UINT_32 y; 
+	INT_32 x;
+	INT_32 y; 
 } POINT;
 
 typedef struct _RECT {
@@ -21,6 +21,18 @@ typedef struct _RECT {
 	UINT_32 width;
 	UINT_32 height;   
 } RECT;
+
+
+typedef struct _WINDOW_OBJECT {
+	RECT             rect;
+	BOOL             clickable;
+	const INT_8*     text;
+	union {
+		BOOL         pressed;
+		UINT_32      slide_bar_distance;
+		const INT_8* text_field_capture_text;
+	}
+} WINDOW_OBJECT;
 
 typedef struct _TITLE_BAR {
 	RECT    rect;
@@ -39,14 +51,17 @@ typedef struct _WINDOW {
 	BORDER_FRAME*   border_frame;
 	const INT_8*    window_name;
 	struct _WINDOW* children;
-	UINT_32         how_many_children;  
+	UINT_32         how_many_children; 
+	UINT_32         z_order; 
 	UINT_8*         buffer;
+	WINDOW_OBJECT*  window_objects;
 } WINDOW;
 
-void init_desktop(void);
-BOOL register_window(WINDOW* wnd, const INT_8* name, UINT_32 width, UINT_32 height, POINT* up_left);
-void draw_window(WINDOW* wnd, UINT_8* framebuffer);
-void window_manager(INT_8* report_packet, UINT_8 event_type, INT_32 x, INT_32 y);
+extern void _sse_blit_off_screen_to_screen(UINT_8* src, UINT_8* trg, UINT_32 blocks_of_16_in_width);
+void        init_desktop(void);
+BOOL        register_window(WINDOW* wnd, const INT_8* name, UINT_32 width, UINT_32 height, POINT* up_left);
+void        draw_window(WINDOW* wnd, UINT_8* framebuffer);
+void        window_manager(INT_8* report_packet, UINT_8 event_type, INT_32 x, INT_32 y, UINT_8* ioctl_payload);
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
