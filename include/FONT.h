b352f6fa0dff1b73d4820218cbcf9b925a896eb4
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2020-01-01 17:59:01 +0100

    Add files via upload

---
+++ include/FONT.h
@@ -0,0 +1,13 @@
+#ifndef _FONT__H__
+#define _FONT__H__
+
+#include "LiBOS_CORE.h"
+#include "WINDOW.h"
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+void draw_font(INT_8 character, WINDOW* wnd, UINT_32 relative_x_on_window, UINT_32 relative_y_on_window);
+
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#endif //! _FONT__H__
